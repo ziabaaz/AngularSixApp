42dce37509c794ed6c12f6f2b6c8cbbd60bb526c
Rahman,Zia Ul <ziaul.rahman@honeywell.com>
2019-03-07 23:39:56 +0530

    Initial Commit

---
+++ AngularSixApp.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using AngularSixApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AngularSixApp.API.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) {}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
